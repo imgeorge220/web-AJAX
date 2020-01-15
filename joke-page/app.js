3868528bd8c52b45a88b74e3b7d9ffbc02240679
jessicaelee <jessica.e.lee.93@gmail.com>
2020-01-14 17:22:57 -0800

    Add up and down votes

--- joke-page/app.js
+++ joke-page/app.js
@@ -1,13 +1,27 @@
 $(init());
 
-async function init(){
-    for (i = 0; i < 10; i++){
-        let newJoke = `<p>${await grabJoke()}</p>`;
+$(".container").on("click", ".up-button", function (e) {
+    changeScore(e, 1)
+})
+$(".container").on("click", ".down-button", function (e) {
+    changeScore(e, -1)
+})
+
+async function init() {
+    for (i = 0; i < 10; i++) {
+        let newJoke = `<tr><td>${await grabJoke()}</td>
+        <td class="up">
+            <button class="up-button">+1</button>
+        </td>
+        <td class="down">
+            <button class="down-button">-1</button>
+        </td>
+        <td class="score">0</td></tr>`;
         $('.container').append(newJoke);
     }
 }
 
-async function grabJoke(){
+async function grabJoke() {
     let config = {
         headers: {
             'Accept': 'text/plain'
@@ -15,4 +29,10 @@ async function grabJoke(){
     };
     let jokeObj = await axios.get('https://icanhazdadjoke.com/', config);
     return jokeObj.data;
+}
+
+function changeScore(e, num) {
+    let oldScore = +($(e.target).parent().siblings(".score").text());
+    $(e.target).parent().siblings(".score").text(oldScore += num);
+
 }
